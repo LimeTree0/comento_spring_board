9930562bc54f44b538e15c499079ee57246d5b1f
LimeTree0 <bum9516@gmail.com>
2023-09-17 14:40:46 +0900

    개발환경 세팅

---
+++ src/main/java/com/comento/limecoding/LimecodingApplication.java
@@ -0,0 +1,13 @@
+package com.comento.limecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimecodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimecodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comento/limecoding/LimecodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comento.limecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimecodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
